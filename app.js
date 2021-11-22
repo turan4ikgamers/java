a2ca9cabeb7359246a3e7bd5e2110bc8c2dd7f34
Turan <turan@email.com>
2021-11-22 20:31:58 +0500

    turan

---
+++ app.js
@@ -0,0 +1,30 @@
+var name = prompt('Введите Свое Имя')
+console.log(name);
+var years =+prompt('Введите Свой Возраст')
+console.log(years);
+
+
+
+var stp = prompt ('11 + 11= ?')
+console.log(stp); 
+
+var str = prompt('15 * 5 = ?')
+console.log(str); 
+
+var stv = prompt('12 / 12= ?')
+
+console.log(stv); 
+
+var stl = prompt('30 -  11 =?')
+console.log(stl); 74
+
+alert('Если Хотите Узать Правилныe Ответы Зайдите В Консоль')
+
+console.log ('Правилный Ответ' +   (11+11) +   ('  ваш ответ') +  stp);
+console.log ('Правилный Ответ' +   (15*5)  +   ('  ваш ответ' )  +  str);
+console.log ('Правилный Ответ' +   (12/5)  +   ('  ваш ответ' )  +  stv);
+console.log ('Правилный Ответ' +   (30-11) +   ('  ваш ответ') +  stl);
+
+
+
+
